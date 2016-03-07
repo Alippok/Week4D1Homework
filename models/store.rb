b5b269b2e3b4567db22c38d331cce3854dcc3531
A_Lippok <alippok@hotmail.co.uk>
2016-03-07 16:36:01 +0000

    Checks store is respoinding correctly

--- models/store.rb
+++ models/store.rb
@@ -2,13 +2,13 @@ require('pg')
 
 class Store 
 
-  attr_reader(:name, :address, :stock_type, :id)
+  attr_reader :name, :address, :stock_type, :id
 
   def initialize(options)
-    @name = options('name')
-    @address = options('address')
-    @stock_type = options('stock_type')
-    @id = nil || options('id')
+    @name = options["name"]
+    @address = options["address"]
+    @stock_type = options["stock_type"]
+    @id = nil || options["id"]
   end
 
 
--- specs/store_spec.rb
+++ specs/store_spec.rb
@@ -6,14 +6,12 @@ require_relative('../models/store.rb')
 class TestStore < MiniTest::Test
 
   def setup
-  
-  @store = Store.new(name: "Allan's Pets", address:"1/3 Horwich Lane", stock_type: "fish")
-
+   @store = Store.new("name" => "Allan's Pets", "address" =>"1/3 Horwich Lane", "stock_type" =>"fish")
   end
 
   
   def test_name
-
+    assert_equal("Allan's Pets", @store.name)
   end
 
 
